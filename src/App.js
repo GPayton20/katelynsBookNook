11d1a6db9b2caa5e04b7dca8189e11067b7b75bd
Geoffrey <geoffrpayton@gmail.com>
2021-05-15 13:24:40 -0400

    set useState for database objects

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import firebase from './firebase.js'
 
 function App() {
 
-  const dbRef = firebase.database().ref();
+  const dbRefToRead = firebase.database().ref('/toRead');
+  const dbRefCompleted = firebase.database().ref('/completed');
 
   const [booksToRead, setBooksToRead] = useState([]);
   const [booksCompleted, setBooksCompleted] = useState([]);
+  
+  useEffect( () => {
+    dbRefToRead.on('value', response => console.log(response.val()));
+    dbRefCompleted.on('value', response => console.log(response.val()));
+  }, []);
+  
   return (
     <Fragment>
       <h1>Katelyn's Reading List</h1>
