5d4b3efcb29a75e0ba4a93f27196adea7ad6ca3f
Geoffrey <geoffrpayton@gmail.com>
2021-05-17 20:48:05 -0400

    fixed bug where navigation menu overlapped header

--- src/SetGoalForm.js
+++ src/SetGoalForm.js
@@ -1,6 +1,10 @@
 const SetGoalForm = () => {
+
+  const setGoal = (event) => {
+    event.preventDefault();
+  }
   return (
-      <form action="submit" className="set-goal-form">
+      <form action="submit" className="set-goal-form" onSubmit={setGoal}>
       <h2>How many books do you want to read?</h2>
         <input type="number" min="1" step="1" defaultValue="1"/>
         <button>Set goal</button>
