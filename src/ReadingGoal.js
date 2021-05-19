a95798934c59324054996f084ff3694fcaeb5ee0
Geoffrey <geoffrpayton@gmail.com>
2021-05-19 15:09:30 -0400

    changed progress bar styling and added hover effects for buttons

--- src/ReadingGoal.js
+++ src/ReadingGoal.js
@@ -4,16 +4,16 @@ const ReadingGoal = ({ booksCompleted, goal }) => {
   const progress = Math.min((booksCompleted.length / goal), 1);
 
   return (
-    <>
+    <div className="progress-container">
       {progress < 1
-      ? <p>You have read {booksCompleted.length} books. Keep it up!</p>
+        ? <p>You have read <span className="text-block">{booksCompleted.length}</span> books. Keep it up!</p>
       : <p>You have reached your goal. Great job!</p>
       }
       
       <div className="progress-tracker">
         <div className="progress" style={ {transform: `scaleX(${progress})`} }></div>
       </div>
-    </>
+    </div>
   )
 
 }
